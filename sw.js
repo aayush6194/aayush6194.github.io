57c6a90d74c0810ae9f67a3c3d2611e1328f0541
Aayush <aayush6194@gmail.com>
2019-11-28 01:22:06 -0600

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-87cf8aaa4249a751a04a.js"
+    "url": "webpack-runtime-4e8adb5b7102c9c7e8bb.js"
   },
   {
     "url": "app-779340ad65a630366f39.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "5ef783f5d39356ccf464fdc1502925bd"
+    "revision": "d2c85e555093a78bfd1441ec99eb06d2"
   },
   {
     "url": "component---src-pages-404-js.b115cd220483bdc55451.css"
